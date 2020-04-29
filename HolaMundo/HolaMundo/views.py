f61afa4a165586b8a1285bc0379d9b5f135ba6ee
daniellisarri <43813297+daniellisarri@users.noreply.github.com>
2020-04-29 10:30:05 +0200

    Actualización de archivos según Tutorial 6
    Cambio los archivos views.py y saludo.html

--- HolaMundo/HolaMundo/views.py
+++ HolaMundo/HolaMundo/views.py
@@ -2,11 +2,24 @@ from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
 
+class Persona(object):
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
 def saludo(request): # Primera vista
+
+    p1 = Persona("Profesor Juan", "Díaz")
+    fecha_actual = datetime.datetime.now()
+
     doc_externo = open("C:/Users/danie/OneDrive/Escritorio/Proyecto COVID-19/HolaMundo/HolaMundo/templates/saludo.html") # Carga documento externo
+
     plt = Template(doc_externo.read()) # Crear plantilla a partir de documento externo
+
     doc_externo.close() # Cierra documento externo
-    ctx = Context() # Creación del contexto (Datos adicionales que puede usar el template)
+
+    ctx = Context({"nombre":p1.nombre, "apellido":p1.apellido, "fecha":fecha_actual}) # Creación del contexto (Datos adicionales como diccionarios que puede usar el template)
+
     documento = plt.render(ctx) # Renderizar el objeto Template pasandole el contexto
 
     return HttpResponse(documento)
