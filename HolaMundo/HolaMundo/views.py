22a389cdeeb5e955418972dde1e48363572c6039
daniellisarri <43813297+daniellisarri@users.noreply.github.com>
2020-04-30 10:20:19 +0200

    Tutorial 8
    Condicionales, filtros y cargadores de plantillas

--- HolaMundo/HolaMundo/views.py
+++ HolaMundo/HolaMundo/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
+from django.template.loader import get_template
 
 class Persona(object):
     def __init__(self, nombre, apellido):
@@ -14,15 +15,19 @@ def saludo(request): # Primera vista
 
     fecha_actual = datetime.datetime.now()
 
-    doc_externo = open("C:/Users/danie/OneDrive/Escritorio/Proyecto COVID-19/HolaMundo/HolaMundo/templates/saludo.html") # Carga documento externo
+    # doc_externo = open("C:/Users/danie/OneDrive/Escritorio/Proyecto COVID-19/HolaMundo/HolaMundo/templates/saludo.html") # Carga documento externo
 
-    plt = Template(doc_externo.read()) # Crear plantilla a partir de documento externo
+    # plt = Template(doc_externo.read()) # Crear plantilla a partir de documento externo
 
-    doc_externo.close() # Cierra documento externo
+    # doc_externo.close() # Cierra documento externo
 
-    ctx = Context({"nombre":p1.nombre, "apellido":p1.apellido, "fecha":fecha_actual, "temas":temas_del_curso}) # Creación del contexto (Datos adicionales como diccionarios que puede usar el template)
+    doc_externo = get_template('saludo.html')
 
-    documento = plt.render(ctx) # Renderizar el objeto Template pasandole el contexto
+    #ctx = Context({"nombre":p1.nombre, "apellido":p1.apellido, "fecha":fecha_actual, "temas":temas_del_curso}) # Creación del contexto (Datos adicionales como diccionarios que puede usar el template)
+
+    # documento = plt.render(ctx) # Renderizar el objeto Template pasandole el contexto
+
+    documento = doc_externo.render({"nombre":p1.nombre, "apellido":p1.apellido, "fecha":fecha_actual, "temas":temas_del_curso})
 
     return HttpResponse(documento)
 
