5f210b25303a791af100a477f9eeb62d23c2078a
daniellisarri <43813297+daniellisarri@users.noreply.github.com>
2020-05-10 13:57:20 +0200

    Fin tutorial 22. Busqueda en BBDD y muestra de resultados.

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from gestionPedidos.models import Articulos
+
 # Create your views here.
 
 def busqueda_productos(request):
     return render(request, "busqueda_productos.html")
 
 def buscar(request):
-    mensaje = "Articulo buscado: %r" %request.GET["prd"]
+    if request.GET["prd"]:
+        #mensaje = "Articulo buscado: %r" %request.GET["prd"]
+        producto = request.GET["prd"]
+        articulos = Articulos.objects.filter(nombre__icontains=producto)
+        
+        return render(request, "resultados_busqueda.html", {"articulos":articulos, "query":producto})
+    else:
+        mensaje = "No has introducido nada"
+
     return HttpResponse(mensaje)
