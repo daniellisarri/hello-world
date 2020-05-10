ae98da7cc8eac0f2a82c0fc0014bdc599afad8ea
daniellisarri <43813297+daniellisarri@users.noreply.github.com>
2020-05-10 15:34:21 +0200

    Fin tutorial 24, envío correos SMTP

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 from gestionPedidos.models import Articulos
 
+from django.core.mail import send_mail
+from django.conf import settings
+
 # Create your views here.
 
 def busqueda_productos(request):
@@ -26,6 +29,13 @@ def buscar(request):
 
 def contacto(request):
     if request.method=="POST":
+        asunto = request.POST["asunto"]
+        mensaje = request.POST["mensaje"] + "___" + request.POST["email"]
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ["COVIDTESTER.0@gmail.com"]
+
+        send_mail(asunto, mensaje, email_from, recipient_list)
+
         return render(request, "gracias.html")
     
     return render(request, "contacto.html")
